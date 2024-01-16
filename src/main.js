ee0341315f998496fb725a8843f691ad1e82f86c
Maor Arie <maorarye10@gmail.com>
2024-01-16 02:20:34 +0200

    hero background mobile + desktop

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import "../node_modules/modern-normalize/modern-normalize.css";
 import "../styles/style.css";
 import "../styles/utils.css";
-import "../styles/components/header.css";
+import "../styles/components/background.css";
+import "../styles/components/hero.css";
 import "../styles/components/mobile-nav.css";
+import "../styles/components/header.css";
 
 import { mobileNav } from "./utils/mobile-nav";
 
