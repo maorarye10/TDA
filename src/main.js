4b76658528b472140a6cabafd9f55df0cb0d93cc
maorarye10 <maorarye10@gmail.com>
2025-02-09 18:46:44 +0000

    fix of animation bug on the hero section

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 import "../node_modules/modern-normalize/modern-normalize.css";
-import "../styles/style.css";
-import "../styles/utils.css";
-import "../styles/components/background.css";
-import "../styles/components/hero.css";
-import "../styles/components/mobile-nav.css";
-import "../styles/components/header.css";
-import "../styles/components/icon-bar.css";
-import "../styles/components/info.css";
-import "../styles/components/services.css";
-import "../styles/components/gallery.css";
-import "../styles/components/contactAndReviews.css";
-import "../styles/components/contact.css";
-import "../styles/components/reviews.css";
-import "../styles/components/footer.css";
-import "../styles/components/lowerBackground.css";
+import "./styles/style.css";
+import "./styles/utils.css";
+import "./styles/components/background.css";
+import "./styles/components/hero.css";
+import "./styles/components/mobile-nav.css";
+import "./styles/components/header.css";
+import "./styles/components/icon-bar.css";
+import "./styles/components/info.css";
+import "./styles/components/services.css";
+import "./styles/components/gallery.css";
+import "./styles/components/contactAndReviews.css";
+import "./styles/components/contact.css";
+import "./styles/components/reviews.css";
+import "./styles/components/footer.css";
+import "./styles/components/lowerBackground.css";
 
 
 import { nav } from "./utils/nav";
