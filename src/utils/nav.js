8321b3d50ae107afb43d9cf7dd6a3429b1fb5324
Maor Arie <maorarye10@gmail.com>
2025-02-19 04:54:07 +0200

    minor UI changes

--- src/utils/nav.js
+++ src/utils/nav.js
@@ -4,6 +4,13 @@ export const nav = () => {
     const handleLinkClick = (e) => {
         e.preventDefault();
         const sectionName = e.target.dataset.section;
+
+        if (sectionName === "info") {
+            const section = document.querySelector(`.icon-bar`);
+            section.scrollIntoView({behavior: "smooth", block: "start"});
+            return;
+        }
+        
         const section = document.querySelector(`.${sectionName}`);
         section.scrollIntoView({behavior: "smooth", block: "center"});
     };
